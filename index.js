ae1a2953324820e2c76deb70310cdc0e2a0ce8e4
Kibigo <jacksonkibigo99@gmail.com>
2023-07-04 18:56:17 +0300

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function saturdayFun (activity = "roller-skate"){
     return `This Saturday, I want to ${activity}!`;
 }
-saturdayFun("bathe my dog");
+saturdayFun(activity = "bathe my dog");
 
-function mondayWork(activity = "go to the office"){
-    return `This Monday, I will ${activity}.`;
+function mondayWork (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
 }
-mondayWork("work from home");
 
-function wrapAdjective(football="*"){
-    return function (name="special"){
-        return `You are ${football}${name}${football}`;
+function wrapAdjective (star = "*"){
+    return function (name = "a hard worker"){
+        return  `You are ${star}${name}${star}!`
     }
+   
 }
